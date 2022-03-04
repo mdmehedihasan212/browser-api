3daf46eb94a1268afa80e2aea0f9362ca49753a6
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 11:51:36 +0600

    explore js

---
+++ explore.js
@@ -0,0 +1,8 @@
+const id = {
+    name: "ananta", job: "business",
+    makeMoview: function (name) {
+        console.log(name);
+    }
+};
+id.makeMoview("din rat");
+// document.getElementById("name");
