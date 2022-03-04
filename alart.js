513c527257778a0d16a07e8e6e4f354d01034e93
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-03-04 12:50:23 +0600

    add confirm alert

---
+++ alart.js
@@ -0,0 +1,11 @@
+// console.log("alert is ok");
+
+// alert('I am is very lazy person')
+
+const familyClick = () => {
+    alert('family is is verry')
+}
+const friendsClick = () => {
+    confirm('friends is varry varry importan')
+
+}
